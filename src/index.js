b7930a9093f2172fb1b1b05afbf12741615063a5
Stephen <stephenplusplus@users.noreply.github.com>
2019-09-05 18:57:51 -0400

    add header

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ const METADATA_SERVICE = 'http://metadata.google.internal./computeMetadata/v1/in
 http
   .createServer((req, res) => {
     const startTime = Date.now()
-    http.get(METADATA_SERVICE, resp => {
+    http.get(METADATA_SERVICE, {
+      headers: {
+        'Metadata-Flavor': 'Google',
+      },
+    }, resp => {
       const elapsedTime = Date.now() - startTime
       console.log(`Metadata request time: ${elapsedTime}ms`)
       res.end(`Metadata request time: ${elapsedTime}ms`)
